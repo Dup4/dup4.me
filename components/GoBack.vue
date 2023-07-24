3cbba7ed79c56a555f70b8dba2176e6fb37556c0
Dup4 <lyuzhi.pan@gmail.com>
2023-07-24 10:13:44 +0800

    feat: add GoBack component
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ components/GoBack.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+const router = useRouter()
+</script>
+
+<template>
+  <div>
+    <div
+      class="m-3 text-sm btn"
+      @click="router.back()"
+    >
+      Back
+    </div>
+
+    <NuxtLink
+      class="m-3 text-sm btn"
+      to="/"
+    >
+      Home
+    </NuxtLink>
+  </div>
+</template>
